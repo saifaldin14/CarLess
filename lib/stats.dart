f6b2696185a5e09a3e2f993a42d1f97aff086013
saifaldin14 <saifaldindeveloper@gmail.com>
2022-10-01 23:31:37 -0400

    Update stats.dart

--- lib/stats.dart
+++ lib/stats.dart
@@ -20,10 +20,63 @@ class Stats extends StatelessWidget {
             },
           ),
         ),
-        body: Column(children: [
-          ProgressBarGradient(),
-          LineChartSample2(),
-          BarChartSample7()
-        ]));
+        body: Padding(
+            padding: EdgeInsets.all(20),
+            child: ListView(children: [
+              Card(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(15.0),
+                  ),
+                  child: Padding(
+                      padding: EdgeInsets.all(20),
+                      child: Column(children: [
+                        Text(
+                          'Current Level: 5',
+                          style: TextStyle(color: Colors.black, fontSize: 24.0),
+                        ),
+                        ProgressBarGradient(),
+                        Align(
+                          alignment: Alignment.centerRight,
+                          child: Text(
+                            '600 / 750 points',
+                            textAlign: TextAlign.right,
+                            style:
+                                TextStyle(color: Colors.black, fontSize: 15.0),
+                          ),
+                        ),
+                      ]))),
+              Card(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(15.0),
+                  ),
+                  child: Padding(
+                      padding: EdgeInsets.all(20),
+                      child: Column(
+                        children: [
+                          Text(
+                            'CO2 Emission Savings',
+                            style:
+                                TextStyle(color: Colors.black, fontSize: 24.0),
+                          ),
+                          LineChartSample2(),
+                        ],
+                      ))),
+              Card(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(15.0),
+                  ),
+                  child: Padding(
+                      padding: EdgeInsets.all(20),
+                      child: Column(
+                        children: [
+                          Text(
+                            'Weekly Leader Board',
+                            style:
+                                TextStyle(color: Colors.black, fontSize: 24.0),
+                          ),
+                          BarChartSample7()
+                        ],
+                      ))),
+            ])));
   }
 }
